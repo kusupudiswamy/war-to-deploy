f8091b9f27f4f0cf9019b75914ae2fbbc5fae322
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-08-03 13:26:57 +0530

    updated dockerfile path
    updated dockerfile path

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-sudo docker build -t tom .
+
+sudo cp Dockerfile /opt/extract
+sudo cd /opt/extract
+pwd
+sudo docker build -t tom /opt/extract
 sudo docker tag tom kusupudiswamy/venkat:war
 sudo docker push kusupudiswamy/venkat:war
