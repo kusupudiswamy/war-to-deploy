fcf2cca28020968b32480b73735302776e7fab11
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-08-03 13:45:05 +0530

    testing with .

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 sudo cp Dockerfile /opt/extract
 sudo cd /opt/extract
 pwd
-sudo docker build -t tom /opt/extract
+sudo docker build -t tom .
 sudo docker tag tom kusupudiswamy/venkat:war
 sudo docker push kusupudiswamy/venkat:war
