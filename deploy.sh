dc10fbd9849967cf478a8fa79b0d3b6c06f9abdb
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-08-06 17:38:01 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,14 @@
 
 #!/bin/bash
 
-#exists=$(docker service ls|grep tomcat)
-
-#if [! "$exists"];then
- #docker service create --name swamy1 -p 80:80 kusupudiswamy/lucky:war
-  # else
- #docke service update --name swamy1 --image  kusupudiswamy/lucky:war
- #fi
- #!/bin/bash
-
 exists=$(docker service ls|grep tomcat)
 
 if [! "$exists"];then
- sudo docker service create --name swamy1 -p 80:80 kusupudiswamy/lucky:war
+ docker service create --name swamy1 -p 80:80 kusupudiswamy/lucky:war
    else
- sudo docker service update --name swamy --image  kusupudiswamy/lucky:war
+ docke service update --name swamy1 --image  kusupudiswamy/lucky:war
  fi
+ 
+
 
  
