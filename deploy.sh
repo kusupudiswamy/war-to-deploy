ae4c4cd46712c1d3f445d72e3d5f7c41248281d8
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-08-06 16:24:51 +0530

    changed image name

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 exists=$(docker service ls|grep tomcat)
 
 if [! "$exists"];then
- docker service create --name swamy -p 80:80 kusupudiswamy/venkat:war
+ docker service create --name swamy1 -p 80:80 kusupudiswamy/satish:war
    else
- docke service update --name swamy --image  kusupudiswamy/venkat:war
+ docke service update --name swamy1 --image  kusupudiswamy/satish:war
  fi
