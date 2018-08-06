48794820b57a0842443efcae30ec9b6c28b274de
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-08-06 17:00:15 +0530

    changed

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 sudo cp Dockerfile /opt/extract
 sudo cd /opt/extract
 pwd
-sudo docker build -t tom1 .
-sudo docker tag tom1 kusupudiswamy/satish:war
-sudo docker push kusupudiswamy/satish:war
+sudo docker build -t sam /opt/extract
+sudo docker tag sam kusupudiswamy/lucky:war
+sudo docker push kusupudiswamy/lucky:war
